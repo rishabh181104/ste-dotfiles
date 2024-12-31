695395e9a58b4216647f2b1f59835e6aea7890b9
Rishabh <rishabhhaldiya18@gmail.com>
2025-01-01 02:29:30 +0530

    s

--- nvim/.config/nvim/lua/config/lazy.lua
+++ nvim/.config/nvim/lua/config/lazy.lua
@@ -52,16 +52,5 @@ require("lazy").setup({
 			}
 		end
 	},
-	{
-		'jose-elias-alvarez/null-ls.nvim',
-		config = function()
-			local null_ls = require('null-ls')
-			null_ls.setup({
-				sources = {
-					null_ls.builtins.diagnostics.eslint, -- Example
-					null_ls.builtins.formatting.prettier, -- Example
-				},
-			})
-		end
-	},
+	'jose-elias-alvarez/null-ls.nvim',
 })
