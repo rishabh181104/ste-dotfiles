bcb631ca104444790c2dd176f9ab4390209d8f7f
Rishabh <rishabhhaldiya18@gmail.com>
2025-01-01 02:23:24 +0530

    s

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -1,44 +1,59 @@
-vim.opt.signcolumn = 'yes'
+local lspconfig = require('lspconfig')
+local cmp = require('cmp')
+local lspconfig_defaults = lspconfig.util.default_config
 
-local lspconfig_defaults = require('lspconfig').util.default_config
+-- Extend default capabilities
 lspconfig_defaults.capabilities = vim.tbl_deep_extend(
-  'force',
-  lspconfig_defaults.capabilities,
-  require('cmp_nvim_lsp').default_capabilities()
+	'force',
+	lspconfig_defaults.capabilities,
+	require('cmp_nvim_lsp').default_capabilities()
 )
 
+-- LSP key mappings and actions
 vim.api.nvim_create_autocmd('LspAttach', {
-  desc = 'LSP actions',
-  callback = function(event)
-    local opts = {buffer = event.buf}
+	desc = 'LSP actions',
+	callback = function(event)
+		local opts = {buffer = event.buf}
 
-    vim.keymap.set('n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>', opts)
-    vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>', opts)
-    vim.keymap.set('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<cr>', opts)
-    vim.keymap.set('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<cr>', opts)
-    vim.keymap.set('n', 'go', '<cmd>lua vim.lsp.buf.type_definition()<cr>', opts)
-    vim.keymap.set('n', 'gr', '<cmd>lua vim.lsp.buf.references()<cr>', opts)
-    vim.keymap.set('n', 'gs', '<cmd>lua vim.lsp.buf.signature_help()<cr>', opts)
-    vim.keymap.set('n', '<F2>', '<cmd>lua vim.lsp.buf.rename()<cr>', opts)
-    vim.keymap.set({'n', 'x'}, '<F3>', '<cmd>lua vim.lsp.buf.format({async = true})<cr>', opts)
-    vim.keymap.set('n', '<F4>', '<cmd>lua vim.lsp.buf.code_action()<cr>', opts)
-  end,
+		vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+		vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+		vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
+		vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
+		vim.keymap.set('n', 'go', vim.lsp.buf.type_definition, opts)
+		vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+		vim.keymap.set('n', 'gs', vim.lsp.buf.signature_help, opts)
+		vim.keymap.set('n', '<F2>', vim.lsp.buf.rename, opts)
+		vim.keymap.set({'n', 'x'}, '<F3>', function() vim.lsp.buf.format({async = true}) end, opts)
+		vim.keymap.set('n', '<F4>', vim.lsp.buf.code_action, opts)
+	end,
 })
 
-require('lspconfig').gleam.setup({})
-require('lspconfig').ocamllsp.setup({})
+-- Language server setup
+local servers = {
+	pyright = {},  -- Python
+	rust_analyzer = {},  -- Rust
+	gopls = {},  -- Go
+	ts_ls = {},  -- TypeScript/JavaScript
+	html = {},  -- HTML
+	cssls = {},  -- CSS
+	gleam = {},  -- Existing Gleam
+	ocamllsp = {}  -- Existing OCaml
+}
 
-local cmp = require('cmp')
+for server, config in pairs(servers) do
+	lspconfig[server].setup(config)
+end
 
+-- CMP setup
 cmp.setup({
-  sources = {
-    {name = 'nvim_lsp'},
-  },
-  snippet = {
-    expand = function(args)
-      -- You need Neovim v0.10 to use vim.snippet
-      vim.snippet.expand(args.body)
-    end,
-  },
-  mapping = cmp.mapping.preset.insert({}),
+	sources = {
+		{name = 'nvim_lsp'},
+	},
+	snippet = {
+		expand = function(args)
+			vim.fn["vsnip#anonymous"](args.body)  -- Adjust if using a different snippet plugin
+		end,
+	},
+	mapping = cmp.mapping.preset.insert({}),
 })
+
