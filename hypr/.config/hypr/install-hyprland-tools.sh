6a42a47a084e7219a0e337f8cf4ffd23280b971e
Rishabh Haldiya <rishabhhaldiya18@gmail.com>
2025-01-31 03:26:14 +0530

    s

---
+++ hypr/.config/hypr/install-hyprland-tools.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+# List of packages to install
+PACKAGES=(
+	hyprland
+	wl-clipboard
+	wf-recorder
+	rofi
+	wlogout
+	dunst
+	swaybg
+	alacritty
+	hyprcursor
+	hyprlang
+	noto-fonts
+	noto-fonts-emoji
+	hyprpicker
+	nwg-look
+	jq
+	gvfs
+	mpv
+	playerctl
+	pamixer
+	noise-suppression-for-voice
+	NetworkManager
+	NetworkManager-applet
+	neovim
+	brightnessctl
+	power-profiles-daemon
+	waybar
+	papirus-icon-theme
+	ttf-font-awesome
+	starship
+	grim
+	slurp
+)
+
+# Function to install packages
+install_packages() {
+	echo "Updating package list..."
+	sudo zypper refresh
+
+	echo "Installing packages..."
+	sudo zypper install -y "${PACKAGES[@]}"
+
+	echo "Installation complete!"
+}
+
+# Run installation
+install_packages
+
